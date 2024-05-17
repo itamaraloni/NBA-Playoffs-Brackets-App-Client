656982dc6c02f5704b332bcf34eaf3e305e3ae21
Itamar Aloni <itamaraloni@gmail.com>
2024-05-17 14:25:43 +0300

    flask and react integration

--- euro2024/src/App.js
+++ euro2024/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/api/data');
+      const result = await response.json();
+      setData(result.message);
+      console.log("noderski");
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React and Flask</h1>
+        <button onClick={fetchData}>Fetch Data</button>
+        {data && <p>{data}</p>}
       </header>
     </div>
   );
