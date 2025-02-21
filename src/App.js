803ae0f900725033d1cc2b7fe1da0572e315cf46
Itamar Aloni <itamaraloni@gmail.com>
2025-02-21 17:35:00 +0200

    call to team api and prints/small fixes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ function App() {
 
   const fetchData = async () => {
     try {
+      console.log("Making request to api/data...");
       const response = await fetch('http://127.0.0.1:5000/api/data');
       const result = await response.json();
       setData(result.message);
-      console.log("noderski");
+      console.log("Finished Request");
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const fetchTeam = async (teamName) => {
+    try {
+      console.log(`Making request to api/teams/${teamName}...`);
+      const response = await fetch(`http://127.0.0.1:5000/api/teams/${teamName}`);
+      const result = await response.json();
+      // setData(result.message);
+      console.log(`Finished Request: ${result.message}`);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -18,7 +31,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React and Flask</h1>
-        <button onClick={fetchData}>Fetch Data</button>
+        <button onClick={() => fetchData()}>Fetch Data</button>
+        <button onClick={() => fetchTeam('Celtics')}>Fetch Team</button>
         {data && <p>{data}</p>}
       </header>
     </div>
