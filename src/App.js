62dcbc6c3ced45252699854a8579bd8da74d640e
Itamar Aloni <itamaraloni@gmail.com>
2025-02-22 18:18:14 +0200

    api for user

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import React, { useState } from 'react';
 function App() {
   const [data, setData] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      console.log("Making request to api/data...");
-      const response = await fetch('http://127.0.0.1:5000/api/data');
-      const result = await response.json();
-      setData(result.message);
-      console.log("Finished Request");
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
   const fetchTeam = async (teamName) => {
     try {
       console.log(`Making request to api/teams/${teamName}...`);
-      const response = await fetch(`http://127.0.0.1:5000/api/teams/${teamName}`);
-      const result = await response.json();
-      // setData(result.message);
-      console.log(`Finished Request: ${result.message}`);
+      const response = await fetch(`http://127.0.0.1:5000/api/users/${teamName}`);
+      const data = await response.json();
+      if (response.ok) {
+        setData(data.email);
+        console.log(`received email from server: ${data.email}`); // Access the email here
+      } else {
+        console.error('Error fetching email');
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -31,8 +23,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React and Flask</h1>
-        <button onClick={() => fetchData()}>Fetch Data</button>
-        <button onClick={() => fetchTeam('Celtics')}>Fetch Team</button>
+        <button onClick={() => fetchTeam('u1')}>Fetch Team</button>
         {data && <p>{data}</p>}
       </header>
     </div>
