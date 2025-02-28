35652eba72fbaa8f07bd9885eda2bd7e99525626
Itamar Aloni <itamaraloni@gmail.com>
2025-02-28 17:31:50 +0200

    add general use button

--- src/App.js
+++ src/App.js
@@ -19,11 +19,28 @@ function App() {
     }
   };
 
+  const generalUse = async () => {
+    try {
+      console.log(`Making request to api/general_use...`);
+      const response = await fetch(`http://127.0.0.1:5000/api/general_use`);
+      const data = await response.json();
+      if (response.ok) {
+        // setData(data.email);
+        console.log(`received email from server: ${data.name}`); // Access the email here
+      } else {
+        console.error('Error fetching email');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React and Flask</h1>
         <button onClick={() => fetchTeam('u1')}>Fetch Team</button>
+        <button onClick={() => generalUse()}>General Use</button>
         {data && <p>{data}</p>}
       </header>
     </div>
