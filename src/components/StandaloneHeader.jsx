6d0ac6455ec5780e42be7833235a3b381ebe18a9
darchock <darchock1@gmail.com>
2025-03-14 20:11:34 +0200

    Created minimal header for stand alone pages; create league\player

---
+++ src/components/StandaloneHeader.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  useTheme
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Logout as LogoutIcon } from '@mui/icons-material';
+import ThemeToggle from '../theme/ThemeToggle';
+
+const StandaloneHeader = ({ title, onLogout, showLogout = true }) => {
+  const theme = useTheme();
+
+  return (
+    <AppBar position="static" color="default" elevation={1}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {title || 'NBA Playoffs Predictor'}
+        </Typography>
+        
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <ThemeToggle />
+          
+          <Button 
+            component={Link} 
+            to="/dashboard" 
+            variant="contained" 
+            color="primary"
+            sx={{ ml: 1 }}
+          >
+            Home
+          </Button>
+
+          {showLogout && (
+            <Button
+              color="inherit"
+              startIcon={<LogoutIcon />}
+              onClick={onLogout}
+              sx={{ ml: 2 }}
+            >
+              Logout
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default StandaloneHeader;
