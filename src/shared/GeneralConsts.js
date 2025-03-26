f4a562a4e306bdf23dd5ffad6c49026161c896b1
darchock <darchock1@gmail.com>
2025-03-26 22:37:39 +0200

    Created src/shared directory Created GeneralConsts.js to consist constants used across the app

---
+++ src/shared/GeneralConsts.js
@@ -0,0 +1,42 @@
+// NBA Teams
+export const NBA_TEAMS = [
+    'Boston Celtics',
+    'Oklahoma City Thunder',
+    'Cleveland Cavaliers',
+    'Los Angels Lakers',
+    'Denver Nuggets',
+    'Golden State Warriors',
+    'New York Knicks',
+    'Minnesota Timberwolves',
+    'Milwaukee Bucks'
+  ];
+
+// MVP Candidates
+export const MVP_CANDIDATES = [
+    'Shai Gilgeous-Alexander',
+    'Jayson Tatum',
+    'Donovan Mitchell',
+    'Jaylen Brown',
+    'Nikola Jokić',
+    'Stephen Curry',
+    'Luka Dončić',
+    'Jalen Williams',
+    'LeBron James',
+    'Darius Garland',
+    'Evan Mobley',
+    'Jalen Brunson',
+    'Giannis Antetokounmpo',
+    'Anthony Edwards',
+    'Kevin Durant',
+    'Damian Lillard',
+    'Other'
+  ];
+
+// Player avatars
+export const PLAYER_AVATARS = [
+    { id: "1", src: '/resources/player-avatars/steph.png', alt: 'Stephan Curry' },
+    { id: "2", src: '/resources/player-avatars/jokic.png', alt: 'Nikola Jokic' },
+    { id: "3", src: '/resources/player-avatars/deni.png', alt: 'Deni Avdija' },
+    { id: "4", src: '/resources/player-avatars/casspi.png', alt: 'Omri Casspi' },
+    { id: "5", src: '/resources/player-avatars/draymond.png', alt: 'Draymond Green' }
+  ];
